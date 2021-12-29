fb91f97d9e6a204185faee8c5f8ccf8d0797dcdb
Taiwo <famaktaiwo7@gmail.com>
2021-12-29 09:07:31 +0100

    first commit

---
+++ CalculatorApp/Calculator.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace CalculatorApp
+{
+    public class Calculator
+    {
+        public double firstnum { get; set; }
+        public double secondnum { get; set; }
+        public double result { get; set; }
+        public string operand { get; set; }
+    }
+}
---
+++ CalculatorApp/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace CalculatorApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //declare variables
+            double firstnum;
+            double secondnum;
+            double result;
+            string operand;
+
+            Console.WriteLine("Enter a number");
+            firstnum = Double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter an operator: +, -, *, /");
+            operand = Console.ReadLine();
+
+            Console.WriteLine("Enter second number");
+            secondnum = Double.Parse(Console.ReadLine());
+
+            if (operand == "+")
+            {
+                result = firstnum + secondnum;
+                Console.WriteLine($"Your result: {firstnum} + {secondnum} = " + (firstnum + secondnum));
+            }
+
+            if (operand == "-")
+            {
+                result = firstnum - secondnum;
+                Console.WriteLine($"Your result: {firstnum} - {secondnum} = " + (firstnum - secondnum));
+            }
+
+            if (operand == "*")
+            {
+                result = firstnum * secondnum;
+                Console.WriteLine($"Your result: {firstnum} * {secondnum} = " + (firstnum * secondnum));
+            }
+
+            if (operand == "/")
+            {
+                result = firstnum / secondnum;
+                Console.WriteLine($"Your result: {firstnum} / {secondnum} = " + (firstnum / secondnum));
+            }
+
+
+            //wait for keypress
+            Console.ReadKey();
+
+        }
+    }
+}
